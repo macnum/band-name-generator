d71522bef10828dcf7fb71a4dac44edaf4258700
Adebayo Damilola <75548362+macnum@users.noreply.github.com>
2022-03-13 22:55:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+user_name = input("What is your name\n")
+print("Hello " + user_name)
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What is your pet name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print(city + " " + pet_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
